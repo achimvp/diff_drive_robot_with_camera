ea405db449f312c88ec8ad048328331e86f6aae5
achimvp <prittwitza@gmail.com>
2026-06-04 18:31:18 +0200

    feat(rl-env): ROS2 communication node for interfacing control policy

--- rl/scripts/ros_control_node.py
+++ rl/scripts/ros_control_node.py
@@ -8,12 +8,12 @@ class RosControlNode(Node):
         self.get_logger().info("ROS Control Node has been initialized.")
 
         self.create_timer(0.1, self.timer_callback) 
-        self.ctrl_pub = self.create_publisher(Float64MultiArray, "/boxbot/action", 10)
+        self.ctrl_pub = self.create_publisher(Float64MultiArray, "/rl/boxbot_action", 10)
         self._init_time = self.get_clock().now()
     
     def timer_callback(self):
         self.get_logger().info("Timer callback executed.")
-        self.ctrl_pub.publish(Float64MultiArray(data=[1.0, 0.0, 0.0, 0.0]))  # Example command to move forward
+        self.ctrl_pub.publish(Float64MultiArray(data=[1.0, 0.0, 1.0, 1.0]))  # Example command to move forward
         if (self.get_clock().now() - self._init_time).nanoseconds > 1e10:  # Run for 10 seconds
             self.get_logger().info("Shutting down ROS Control Node after 10 seconds.")
             raise SystemExit
